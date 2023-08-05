5cf7e9d19134bfd6d59378ac628337f20b6d5166
Arvin Dela Rosa <delarosafx@gmail.com>
2023-08-05 16:28:22 +0800

    added search suggestions feature

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const APIkey = 'f225ed595595cbc33fb4514b662d696d';
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric'
 
+async function fetchCitySuggestions(inputText) {
+    const geoAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${inputText}&limit=5&appid=${APIkey}`
+
+    try {
+        const response = await fetch(geoAPI);
+        if (!response.ok) {
+            throw new Error('Failed to load suggestions');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return[];
+    }
+
+}
 
 //Default city set to Manila
 const input = document.querySelector('input');
@@ -19,13 +35,23 @@ input.addEventListener('keyup', () => {
     }
 })
 
+input.addEventListener('input', async (event) => {
+    const inputText = event.target.value.trim();
+    if (inputText.length > 2) {
+        const suggestions = await fetchCitySuggestions(inputText);
+        displaySuggestions(suggestions);
+    } else {
+        document.querySelector('.suggestions').innerHTML = ''
+    }
+});
+
 async function checkWeather(){
     try {
         const response = await fetch(apiURL + `&q=${input.value}` + `&appid=${APIkey}`)
         const data = await response.json();
 
         const temp = document.querySelector('.temp');
-        temp.textContent = `${data.main.temp}°`
+        temp.textContent = `${data.main.temp}°ᶜ`
 
         const city = document.querySelector('.city');
         city.textContent = data.name;
@@ -40,10 +66,22 @@ async function checkWeather(){
         clouds.setAttribute('src',`images/${data.weather[0].main.toLowerCase()}.png`)
 
         input.value = '';
-        
-        console.log(data);
-        console.log(data.weather[0].main.toLowerCase());
     } catch (error) {
         console.log('Error occured:', error.message);
     }
+}
+
+function displaySuggestions (suggestions) {
+    const suggestionsContainer = document.querySelector('.suggestions');
+    suggestionsContainer.innerHTML = '';
+
+    suggestions.forEach(({name, country}) => {
+        const suggestionElement = document.createElement('li');
+        suggestionElement.textContent = `${name}, ${country}`;
+        suggestionElement.addEventListener('click', () => {
+            checkWeather();
+            suggestionsContainer.innerHTML = '';
+        });
+        suggestionsContainer.appendChild(suggestionElement);
+    });
 }
